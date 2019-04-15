7372103ff96bb7f6a3d97b5b53c7cab9dc7d0e4d
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-15 12:23:13 -0400

    add custom color to each player

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,11 +3,18 @@ import classnames from 'classnames';
 
 export default class Box extends React.Component {
   render() {
+    const playerClass = this.props.value ? `player-${this.props.value}` : '';
     return (
       <button
-        className={classnames('box-wrapper', 'float-left', 'text-center', {
-          highlighted: this.props.highlighted
-        })}
+        className={classnames(
+          'box-wrapper',
+          'float-left',
+          'text-center',
+          playerClass,
+          {
+            highlighted: this.props.highlighted
+          }
+        )}
         onClick={() => this.props.onClick()}
       >
         {this.props.value}
