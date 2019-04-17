d902d86e2cbce1ef6790a548935409dc8c53a415
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-17 08:37:36 -0400

    refactoring code using destructuring for props

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -3,19 +3,20 @@ import Box from './Box';
 
 export default class Panel extends React.Component {
   render() {
+    const { winningLine, boxes, onClick } = this.props;
     return (
       <div className="panel-inner-container">
         {[...Array(9)].map((e, i) => {
           let highlighted = false;
-          if (this.props.winningLine && this.props.winningLine.includes(i)) {
+          if (winningLine && winningLine.includes(i)) {
             highlighted = true;
           }
           return (
             <Box
               key={i}
-              value={this.props.boxes[i]}
+              value={boxes[i]}
               highlighted={highlighted}
-              onClick={() => this.props.onClick(i)}
+              onClick={() => onClick(i)}
             />
           );
         })}
