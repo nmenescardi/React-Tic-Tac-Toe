b8e6e83f4ca10eb430a504620f03d64921f6fc2e
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-18 07:11:49 -0400

    Updates

---
+++ precache-manifest.e9177f9b5f635794ca3eb3fe8239496e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02806f2f762087ad7298",
+    "url": "/React-Tic-Tac-Toe/static/js/runtime~main.0bda7406.js"
+  },
+  {
+    "revision": "1b05bbf07757033a5ede",
+    "url": "/React-Tic-Tac-Toe/static/js/main.35877f6a.chunk.js"
+  },
+  {
+    "revision": "5df67526c40f38e300a2",
+    "url": "/React-Tic-Tac-Toe/static/js/2.09a85d95.chunk.js"
+  },
+  {
+    "revision": "1b05bbf07757033a5ede",
+    "url": "/React-Tic-Tac-Toe/static/css/main.fbedb12c.chunk.css"
+  },
+  {
+    "revision": "5df67526c40f38e300a2",
+    "url": "/React-Tic-Tac-Toe/static/css/2.eec06867.chunk.css"
+  },
+  {
+    "revision": "ea2fbc07dd487e496f500110dd0f293b",
+    "url": "/React-Tic-Tac-Toe/index.html"
+  }
+];
